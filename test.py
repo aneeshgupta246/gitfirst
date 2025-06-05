6f919f72c4a68aa885c99bdb6cf649497660ba62
Aneesh Gupta <155018103+aneeshgupta246@users.noreply.github.com>
2025-06-05 18:16:38 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,2 @@
-print("hello world !!")   -> writen by d1
-print ("thi line is added from github")   -> writen by d2
+print("hello world !!")   -> written by d1
+print ("thi line is added from github")   -> written by d2
