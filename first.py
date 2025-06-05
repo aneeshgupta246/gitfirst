86ed59e04009ad1bb5dc1eb0d4b3560d73fa9a86
Aneesh Gupta <aneeshgupta01@outlook.com>
2025-06-05 16:36:49 +0530

    this is new for me

--- first.py
+++ first.py
@@ -1,3 +1,10 @@
 print("hello world !!")
 # This is a simple Python script that prints a message to the console.
 # It serves as a basic example of how to write and run Python code.
+
+print("hello world !!")
+print("hello world !!")
+print("hello world !!")
+print("hello world !!")
+print("hello world !!")
+print("hello world !!")
